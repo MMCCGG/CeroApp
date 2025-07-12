d66518a2973163948e60b415a671e4bc40762c90
MMCCGG <marianocgarcia@hotmail.com>
2025-07-12 07:43:36 +0200

    Esqueleto backend

--- src/main/java/ceroapp/resttasks/modelo/entities/Task.java
+++ src/main/java/ceroapp/resttasks/modelo/entities/Task.java
@@ -18,7 +18,7 @@ public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     private String title;
     private boolean completed;
 }
