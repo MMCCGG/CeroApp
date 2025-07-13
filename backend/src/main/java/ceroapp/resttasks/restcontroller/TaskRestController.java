0d43117d8e177a04189e9a55dc3dedfd04bf40dd
MMCCGG <marianocgarcia@hotmail.com>
2025-07-13 17:08:42 +0200

    TaskRestController modified and ceroapp/config working properly

--- backend/src/main/java/ceroapp/resttasks/restcontroller/TaskRestController.java
+++ backend/src/main/java/ceroapp/resttasks/restcontroller/TaskRestController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +19,7 @@ import ceroapp.resttasks.modelo.entities.Task;
 import ceroapp.resttasks.modelo.services.TaskService;
 
 @RestController
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RequestMapping("/tasks")
 public class TaskRestController {
 	@Autowired private TaskService tservice;
