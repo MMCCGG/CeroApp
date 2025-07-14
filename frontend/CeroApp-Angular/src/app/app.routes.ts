641ebb49b912742654b8cd174c992a87d145a9ad
MMCCGG <marianocgarcia@hotmail.com>
2025-07-14 18:42:39 +0200

    AppRoutes redirect to Task-List

--- frontend/CeroApp-Angular/src/app/app.routes.ts
+++ frontend/CeroApp-Angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { TaskList } from "./tasks/pages/task-list/task-list";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "tasks" },
+  { path: "tasks", component: TaskList },
+  { path: "**", redirectTo: "tasks" },
+];
