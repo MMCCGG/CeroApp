aa477de0b987d3f3989e50a80afb67d7ffc9c0df
MMCCGG <marianocgarcia@hotmail.com>
2025-07-16 19:10:29 +0200

    Add task-form

--- frontend/CeroApp-Angular/src/app/tasks/pages/task-form/task-form.ts
+++ frontend/CeroApp-Angular/src/app/tasks/pages/task-form/task-form.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { TaskService } from "../../services/task.service";
+import { TaskModel } from "../../models/task.model";
 
 @Component({
-  selector: 'app-task-form',
-  imports: [],
-  templateUrl: './task-form.html',
-  styleUrl: './task-form.css'
+  selector: "app-task-form",
+  imports: [ReactiveFormsModule],
+  templateUrl: "./task-form.html",
+  styleUrl: "./task-form.css",
 })
 export class TaskForm {
+  private fb = inject(FormBuilder);
+  private taskService = inject(TaskService);
+  private router = inject(Router);
 
+  taskForm = this.fb.group({
+    title: ["", [Validators.required, Validators.minLength(3)]],
+    completed: [false],
+  });
+
+  guardar() {
+    if (this.taskForm.invalid) return;
+
+    const task: TaskModel = this.taskForm.value as TaskModel;
+
+    this.taskService.insert(task).subscribe({
+      next: () => this.router.navigate(["/"]),
+      error: (err) => console.error("Error al guardar", err),
+    });
+  }
 }
